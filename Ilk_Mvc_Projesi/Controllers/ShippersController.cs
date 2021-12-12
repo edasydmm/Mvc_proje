f1017fa7face3bea0b650799098f930ca9b054ba
eda <sydmeda12@gmail.com>
2021-12-12 17:29:29 +0200

    Ilk_Mvc_projesi

---
+++ Ilk_Mvc_Projesi/Controllers/ShippersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Ilk_Mvc_Projesi.Models;
+using Ilk_Mvc_Projesi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Ilk_Mvc_Projesi.Controllers
+{
+    public class ShipperController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public ShipperController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        //[HttpGet]
+        public IActionResult Index()
+        {
+            var data = _context.Shippers
+               .Include(x => x.Orders)
+               .OrderBy(x => x.ShipperId)
+                .ToList();
+
+            return View(data);
+        }
+        public IActionResult Detail(int? ıd)
+        {
+
+            var Shipper = _context.Shippers
+                .Include(x => x.Orders)
+                .FirstOrDefault(x => x.ShipperId == ıd);
+            if (Shipper == null)
+                return RedirectToAction(nameof(Index));
+            return View(Shipper);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ShipperViewModel model)
+        {
+           
+                return View();
+            }
+        }
+
+
+
+    }
+
---
+++ Ilk_Mvc_Projesi/ViewModels/ShipperViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ilk_Mvc_Projesi.ViewModels
+{
+    public class ShipperViewModel
+    {
+        [Display(Name = "Şirket Adı")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Numara")]
+        public string  Phone  { get; set; }
+
+        public int ShipperID{ get; set; }
+    }
+}
