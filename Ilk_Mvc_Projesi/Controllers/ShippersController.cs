cb346f94aa570874f9d93709d2e43e3195b82540
edasydmm <75896801+edasydmm@users.noreply.github.com>
2021-12-13 20:02:42 +0300

    product

--- Ilk_Mvc_Projesi/Controllers/ShippersController.cs
+++ Ilk_Mvc_Projesi/Controllers/ShippersController.cs
@@ -47,12 +47,76 @@ namespace Ilk_Mvc_Projesi.Controllers
         [HttpPost]
         public IActionResult Create(ShipperViewModel model)
         {
-           
-                return View();
+            if (!ModelState.IsValid)
+            {
+                return View(model);
             }
+
+            var shipper = new Shipper()
+            {
+                //CategoryId = 1,//hata versin diye yazdık
+                CompanyName = model.CompanyName,
+                Phone = model.Phone
+            };
+            _context.Shippers.Add(shipper);
+
+            try
+            {
+                _context.SaveChanges();
+                return RedirectToAction("Detail", new { id = shipper.ShipperId });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"{model.CompanyName} eklenirken bir hata oluştu. Tekrar deneyiniz");
+                return View(model);
+            }
+        }
+        public IActionResult Update(int? id)
+        {
+            var shipper = _context.Shippers.FirstOrDefault(x => x.ShipperId == id);
+            if (shipper == null) return RedirectToAction(nameof(Index));
+
+            var model = new ShipperViewModel()
+            {
+                ShipperID = shipper.ShipperId,
+                CompanyName = shipper.CompanyName,
+                Phone = shipper.Phone
+
+            };
+
+            return View(model);
         }
 
 
+        [HttpPost]
+        public IActionResult Update(ShipperViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
 
+            var shipper = _context.Shippers.FirstOrDefault(x => x.ShipperId== model.ShipperID);
+
+            try
+            {
+                shipper.CompanyName = model.CompanyName;
+                shipper.Phone = model.Phone;
+               
+                _context.Shippers.Update(shipper);
+
+                _context.SaveChanges();
+                return RedirectToAction("Detail", new { id = shipper.ShipperId });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"{model.CompanyName} güncellenirken bir hata oluştu. Tekrar deneyiniz");
+                return View(model);
+            }
+        }
     }
+}
+
+
+
 
